c31f0f804fbcc277d7363dcd1508a4d4604e26d6
Dzhem Rushen Syuleyman <122823771+superGemHere@users.noreply.github.com>
2024-10-22 15:36:34 +0300

    feat: create category box

--- fitness/lib/pages/home.dart
+++ fitness/lib/pages/home.dart
@@ -10,8 +10,31 @@ class HomePage extends StatelessWidget {
       appBar: appBar(),
       backgroundColor: Colors.white,
       body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          _searchField()
+          _searchField(),
+          SizedBox(height: 40),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(left: 20),
+                child: Text(
+                  'Category',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 18,
+                    fontWeight: FontWeight.w600
+                  ),
+                ),
+              ),
+              SizedBox(height: 15),
+              Container(
+                height: 150,
+                color: Colors.green,
+              )
+            ],
+          ) 
         ],
       ),
     );
