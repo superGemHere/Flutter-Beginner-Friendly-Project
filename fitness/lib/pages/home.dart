b42cf90765630641baba6c165e9cb498f84f2550
Dzhem Rushen Syuleyman <122823771+superGemHere@users.noreply.github.com>
2024-10-27 18:46:00 +0200

    feat: implement popular section

--- fitness/lib/pages/home.dart
+++ fitness/lib/pages/home.dart
@@ -33,25 +33,96 @@ class HomePage extends StatelessWidget {
           SizedBox(height: 40),
           _dietSection(),
           SizedBox(height: 40),
-          Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Text(
-                  'Popular',
-                  style: TextStyle(
-                    color: Colors.black,
-                    fontSize: 18,
-                    fontWeight: FontWeight.w600
-                  ),
-                ),
-              ),
-            ],
-          )
+          _popularSection(),
+          SizedBox(height: 40),
         ],
       ),
     );
+  }
+
+  Column _popularSection() {
+    return Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(
+                'Popular',
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 18,
+                  fontWeight: FontWeight.w600
+                ),
+              ),
+            ),
+            SizedBox(height: 15),
+            ListView.separated(
+            itemCount: popularDiets.length,
+            shrinkWrap: true,
+            separatorBuilder: (context, index) => SizedBox(height: 25,),
+            padding: EdgeInsets.only(
+              left: 20,
+              right: 20,
+            ),
+            itemBuilder: (context, index) {
+              return Container(
+                height: 100,
+                decoration: BoxDecoration(
+                  color: popularDiets[index].boxIsSelected ?  Colors.white : Colors.transparent,
+                  borderRadius: BorderRadius.circular(16),
+                  boxShadow: popularDiets[index].boxIsSelected ? [
+                    BoxShadow(
+                      color: Color(0xff1D1617).withOpacity(0.07),
+                      blurRadius: 40,
+                      spreadRadius: 0
+                    )
+                  ] : []
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    SvgPicture.asset(popularDiets[index].iconPath,
+                    width: 65,
+                    height: 65,
+                    ),
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      // crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          popularDiets[index].name,
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 16,
+                            fontWeight: FontWeight.w500,
+                          ),
+                        ),
+                        Text(
+                          popularDiets[index].level + ' | ' + popularDiets[index].duration + ' | ' + popularDiets[index].calorie,
+                          style: TextStyle(
+                            color: Color(0xff7B6F72),
+                            fontSize: 13,
+                            fontWeight: FontWeight.w400,
+                          ),
+                        ),
+                      ],
+                    ),
+                    GestureDetector(
+                      onTap: (){},
+                      child: SvgPicture.asset(
+                        'assets/icons/button.svg',
+                        width: 30,
+                        height: 30,
+                      ),
+                    ),
+
+                  ],
+                ),
+              );
+            },
+          )
+          ],
+        );
   }
 
   Column _dietSection() {
